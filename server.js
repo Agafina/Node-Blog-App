0088355e56b7cdea436fc78d0476446375d2a0ba
Agafina <98034773+Agafina@users.noreply.github.com>
2024-02-19 21:23:16 +0100

    here i added the post request

--- server.js
+++ server.js
@@ -1,22 +1,48 @@
 const express = require('express')
 const morgan = require('morgan') 
+const blogs = require('./models/blogs')
+const mongoose = require('mongoose');
+const Blog = require('./models/blogs');
 
 const app = express();
+
+const dbURI = 'mongodb+srv://agafina:agafina12@cluster0.2iuve6p.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(dbURI)
+.then((result) =>  {
+    app.listen(3000)
+    console.log("Connected to DB")
+})
+.catch(err => console.log(err))
+
+
 app.set('view engine' ,'ejs')
-app.listen(3000)
-app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'));
 app.use(morgan('dev'))
 
+
+
 app.get( '/',(req , res) => {
-    const blogs = [
-        {title: 'The main man' , snippet: 'Looking forward to life with my brothers'},
-        {title: 'The main man' , snippet: 'Looking forward to life with my brothers'},
-        {title: 'The main man' , snippet: 'Looking forward to life with my brothers'}
-    ]
-    res.render('index', {title : 'Home', blogs})
+    res.redirect('/blogs')
 })
 app.get('/about' ,(req , res) => {
     res.render('about', {title : 'About' })
+})
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({createdAt : -1})
+    .then((result) => {
+        res.render('index', {title: 'All Blogs', blogs: result})
+    })
+})
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body)
+    blog.save()
+    .then(result => {
+        res.redirect('/blogs')
+    })
+    .catch(err => {
+        console.log(err)
+    })
 })
 app.get('/blogs/create' ,(req , res) => {
     res.render('create', {title : 'Create new blog'})
