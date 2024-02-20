983f6ad2f76f72f29d689e50d71d8919ba0766da
Agafina <98034773+Agafina@users.noreply.github.com>
2024-02-20 23:58:29 +0100

    added a blog router

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const morgan = require('morgan') 
-const blogs = require('./models/blogs')
 const mongoose = require('mongoose');
-const Blog = require('./models/blogs');
+const blogRouters = require('./routers/blogRouter')
+
 
 const app = express();
 
@@ -28,42 +28,8 @@ app.get( '/',(req , res) => {
 app.get('/about' ,(req , res) => {
     res.render('about', {title : 'About' })
 })
-app.get('/blogs', (req, res) => {
-    Blog.find().sort({createdAt : -1})
-    .then((result) => {
-        res.render('index', {title: 'All Blogs', blogs: result})
-    })
-})
-app.post('/blogs', (req, res) => {
-    const blog = new Blog(req.body)
-    blog.save()
-    .then(result => {
-        res.redirect('/blogs')
-    })
-    .catch(err => {
-        console.log(err)
-    })
-})
-app.get('/blogs/create' ,(req , res) => {
-    res.render('create', {title : 'Create new blog'})
-})
-
-app.get('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findById(id)
-        .then(result => {
-            res.render('details', {blog: result , title: 'Blog Details'});
-        })
-        .catch(err => console.log(err))
-})
-app.delete('/blogs/:id' ,(req ,res) => {
-    const id = req.params.id;
-    Blog.findByIdAndDelete(id)
-    .then(result =>{
-        res.json({ redirect: '/blogs'})
-    })
-    .catch(err => console.log(err))
-})
+// blog routes
+app.use('/blogs', blogRouters)
 
 app.use((req ,res) =>{
     res.status(404).render('404', {title : 'Error page'})
